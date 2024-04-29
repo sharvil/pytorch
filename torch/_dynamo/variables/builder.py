af50e8b3f892c0ec55e60ece8d95b828a9b9f608
ydwu4 <ydwu2014@gmail.com>
2024-04-29 16:42:38 -0700

    [dynamo] support torchbind object input
    ghstack-source-id: 9f38905334dd3c1aecadaa43fd363ea2989aae26
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/124978

--- torch/_dynamo/variables/builder.py
+++ torch/_dynamo/variables/builder.py
@@ -26,6 +26,7 @@ import torch
 
 from torch import SymInt
 from torch._guards import GuardSource, TracingContext
+from torch._higher_order_ops.torchbind import call_torchbind
 from torch._ops import HigherOrderOperator
 from torch._streambase import _EventBase, _StreamBase
 from torch._subclasses.fake_tensor import FakeTensor, is_fake, maybe_get_fake_mode
@@ -154,6 +155,7 @@ from .misc import (
 )
 from .nn_module import FSDPManagedNNModuleVariable, UnspecializedNNModuleVariable
 from .optimizer import OptimizerVariable
+from .script_object import TorchScriptObjectVariable
 
 from .sdpa import SDPAParamsVariable
 from .tensor import (
@@ -887,6 +889,38 @@ class VariableBuilder:
                     user_cls=type(value),
                     user_cls_source=AttrSource(self.source, "__class__"),
                 ),
+            )
+        elif TorchScriptObjectVariable.is_matching_cls(type(value)):
+            from ..source import FlattenScriptObjectSource
+
+            # different torch.ScriptObjects can point to the same underlying value
+            # (but we guarantee that they will `hash()` to the same value if that's the case).
+
+            # Install the guards on the content of value
+            convert_source = FlattenScriptObjectSource(self.source)
+            LazyVariableTracker.realize_all(
+                VariableBuilder(self.tx, convert_source)(value.__obj_flatten__())
+            )
+
+            fake_script_obj = torch._library.fake_class_registry.to_fake_obj(
+                self.tx.output.fake_mode, value
+            )
+
+            proxy = self.tx.output.root_tracer.create_graph_input(
+                re.sub(r"[^a-zA-Z0-9]+", "_", self.name),
+                type(value),
+                source=convert_source,
+            )
+            # setting is_unspecialized=False to not insert a as_tensor call in reconstruct by default
+            # seting example to be real value because these example values will be used
+            # as example_inputs for user compiler.
+            proxy.node.meta["grapharg"] = GraphArg(
+                convert_source, value, False, None, False, fake_script_obj
+            )
+            return TorchScriptObjectVariable.create(
+                proxy,
+                fake_script_obj,
+                source=convert_source,
             )
         else:
             self.install_guards(GuardBuilder.TYPE_MATCH)
@@ -1719,6 +1753,12 @@ def wrap_fx_proxy_cls(
         torch.backends.cuda.can_use_flash_attention,
         torch.backends.cuda.can_use_efficient_attention,
     ]:
+        set_example_value(proxy.node, example_value)
+        return ConstantVariable.create(example_value, **options)
+    elif (
+        isinstance(example_value, (int, float, bool))
+        and proxy.node.target is call_torchbind
+    ):
         set_example_value(proxy.node, example_value)
         return ConstantVariable.create(example_value, **options)
     else:
